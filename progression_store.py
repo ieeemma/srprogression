a9f4bfe84ac27bf5f512de2ba5081c2b303faea9
commentsr <39064859+commentsr@users.noreply.github.com>
2019-02-23 18:19:56 +0000

    Added save() method
    added save() method and added file path arguments

--- progression_store.py
+++ progression_store.py
@@ -20,8 +20,8 @@ class ProgressionStore:
         self.story_difficulty = [0, 0, 0, 0]
         self.story_chapters = [StoryChapter() for i in range(4)]
 
-    def load(self):
-        with open("SpeedRunnerHDProgressionStore", 'rb') as progression_store:
+    def load(self, path):
+        with open(path, 'rb') as progression_store:
             progression_store.read(1)
             self.version = struct.unpack('<i', progression_store.read(4))[0]
             self.xp = struct.unpack('<i', progression_store.read(4))[0]
@@ -37,6 +37,26 @@ class ProgressionStore:
                 self.story_difficulty[i] = struct.unpack('<i', progression_store.read(4))[0]
                 for j in range(4):
                     self.story_chapters[i].levels[j] = struct.unpack('<?', progression_store.read(1))[0]
+
+    def save(self, path):
+        data = b""
+        data += struct.pack('<?', False) # garbage byte at start of file
+        data += struct.pack('<i', self.version)
+        data += struct.pack('<i', self.xp)
+        data += struct.pack('<?', self.played_before)
+        data += struct.pack('<?', self.played_tutorial)
+        data += struct.pack('<i', len(self.unlocks))
+        for unlock in self.unlocks:
+            data += struct.pack('<i', unlock.reward_id)
+            data += struct.pack('<?', unlock.is_unlocked)
+            data += struct.pack('<?', unlock.is_new)
+        for i in range(4):
+            data += struct.pack('<i', self.story_difficulty[i])
+            data += struct.pack('<????', *self.story_chapters[i].levels)
+
+        with open(path, 'wb') as out:
+            out.write(data)
+
 
 # debug prints
 
